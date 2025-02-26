4619b6dfe7ed4dbfd3f8e8967d7d7700ec2dd501
Peniel Ben <penivera655@gmail.com>
2025-02-26 09:20:08 +0100

    Updated URL type

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,7 +1,7 @@
 from pydantic import (BaseModel,
                       BeforeValidator,
                       EmailStr,
-                      Field,)
+                      Field,AnyUrl)
 from typing import Optional,Annotated
 from  app.utils.enums import PhoneStr,AccountTypeEnum
 from app.utils.utils import hash_password
@@ -12,7 +12,7 @@ from app.utils.enums import KycStatusEnum
 
 #NOTE - Base User Schema and response schema
 class BaseUserSchema(BaseModel):
-    profile_pic : Annotated[Optional[str],Field(examples=['https://example/img/user.jpg'],max_length=255)]=None
+    profile_pic : Annotated[Optional[AnyUrl],Field(examples=['https://example/img/user.jpg'],max_length=255)]=None
     username:Annotated[Optional[str],Field(example='username',default='Admin',max_length=30)]
     email: Annotated[EmailStr,Field(examples=['penivera655@gmail.com'])]
     phone_number:Optional[PhoneStr]
