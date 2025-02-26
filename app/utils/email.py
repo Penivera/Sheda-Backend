902f6609948bf5f6625c949af82204011dca2bf1
SpiDher <penivera655@gmail.com>
2025-02-26 19:21:53 +0100

    Upgraded to the current lib structure

--- app/utils/email.py
+++ app/utils/email.py
@@ -1,6 +1,6 @@
 import smtplib
 #from email.mime.application import MIMEApplication
-from email_lib import render_template,send_email
+from email_lib import render_template,EmailSender
 from random import randint
 from datetime import timedelta
  
@@ -9,6 +9,12 @@ from core.dependecies import env
 
 token_expiration = int(VERIFICATION_CODE_EXP_MIN.total_seconds()/60)
 
+email_sender = EmailSender(
+    smtp_host=EMAIL_HOST,
+    smtp_user=EMAIL,
+    smtp_pass=APP_PASS,
+    from_email=EMAIL
+)
         
 async def create_set_send_otp(email:str,user_data:dict=None) -> int:
     otp= str(randint(1000,9999))
@@ -20,13 +26,7 @@ async def create_set_send_otp(email:str,user_data:dict=None) -> int:
         await redis.expire(user_data_prefix.format(email),timedelta(hours=2))
         logger.info('timer set on user data 2 hours')
     otp_text = render_template(env,TEMPLATES['otp'],otp=otp,expiry=token_expiration,support_email=EMAIL)
-    send_otp = await send_email(text=otp_text,
-                                from_email=EMAIL,
-                                to_email=email,
-                                subject='OTP for Verification',
-                                smtp_host=EMAIL_HOST,
-                                smtp_user=EMAIL,
-                                smtp_pass=APP_PASS)
+    send_otp = await email_sender.send_email(text=otp_text,to_email=email,subject='OTP for Verification')
                                 
     if send_otp:
         logger.info('OTP Sent')
@@ -40,13 +40,7 @@ async def send_otp_for_signup(email:str):
     if stored_otp and user_data:
         otp = stored_otp.decode()
         otp_text = render_template(env,TEMPLATES['otp'],otp=otp,expiry=token_expiration,support_email=EMAIL)
-        send_otp = await send_email(text=otp_text,
-                                    from_email=EMAIL,
-                                    to_email=email,
-                                    subject='OTP for Verification',
-                                    smtp_host=EMAIL_HOST,
-                                    smtp_user=EMAIL,
-                                    smtp_pass=APP_PASS)
+        send_otp = await email_sender.send_email(text=otp_text,to_email=email,subject='OTP for Verification',)
         if send_otp:
             logger.info('OTP Resent')
             return True
@@ -55,13 +49,7 @@ async def send_otp_for_signup(email:str):
     await redis.setex(otp_prefix.format(email),timedelta(minutes=2),otp)
     logger.info(f'otp saved to redis for {int(VERIFICATION_CODE_EXP_MIN.total_seconds()/60)}')
     otp_text = render_template(env,TEMPLATES['otp'],otp=otp,expiry=token_expiration,support_email=EMAIL)
-    send_otp = await send_email(text=otp_text,
-                                from_email=EMAIL,
-                                to_email=email,
-                                subject='OTP for Verification',
-                                smtp_host=EMAIL_HOST,
-                                smtp_user=EMAIL,
-                                smtp_pass=APP_PASS)
+    send_otp = await email_sender.send_email(text=otp_text,to_email=email,subject='OTP for Verification',)
     if send_otp:
         logger.info('OTP Sent')
         return True
