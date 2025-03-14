f150c486e3ef646bccec0505a3067e38e39325e8
SpiDher <penivera655@gmail.com>
2025-03-14 02:35:59 +0100

    update to swagger ui

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
 from core.starter import lifespan
-from core.configs import origins
+from core.configs import origins,DEBUG_MODE
 from app.routers import auth_router,user_router,listing_router
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(lifespan=lifespan,title='Sheda Solutions Backend',version='0.1.0',docs_url='/',description='Backend for Sheda Solutions',root_path='/api')
+app = FastAPI(lifespan=lifespan,title='Sheda Solutions Backend',version='0.1.0',docs_url='/',description='Backend for Sheda Solutions',debug=DEBUG_MODE,servers=[
+    {"url": "https://sheda-backend-production.up.railway.app", "description": "Production Server"},
+    {"url": "http://localhost:8000", "description": "Local Development"},
+    ]
+              )
 
 #NOTE - Include Routers
-app.include_router(auth_router.router)
-app.include_router(user_router.router)
-app.include_router(listing_router.router)
+app.include_router(auth_router.router,prefix='/api')
+app.include_router(user_router.router,prefix='/api')
+app.include_router(listing_router.router,prefix='/api')
 
 
 #STUB - Set in full production
