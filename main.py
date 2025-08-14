0ef58584666c17fd33c7a9baa3d65c0fcedf3dc3
Peniel Ben <penivera655@gmail.com>
2025-08-14 20:21:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.routers import auth, listing, user,chat,media
 from core.starter import lifespan
-from core.configs import origins,DEBUG_MODE
+from core.configs import origins,DEBUG_MODE,METHODS,allow_headers
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(lifespan=lifespan,title='Sheda Solutions Backend',version='0.1.0',docs_url='/',description='Backend for Sheda Solutions',debug=DEBUG_MODE,servers=[
@@ -23,8 +23,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=METHODS,
+    allow_headers=allow_headers,
 )
 
 
