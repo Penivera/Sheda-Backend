97d745e84fa836f65164d2c112d2b1e3357f20e1
Penivera <penivera655@gmail.com>
2026-02-16 15:57:46 +0100

    fixed error

--- app/schemas/property_schema.py
+++ app/schemas/property_schema.py
@@ -19,7 +19,7 @@ class PropertyImage(BaseModel):
 class PropertyBase(BaseModel):
     title: str
     description: str
-    blockchain_property_id: Optional[int] = None
+    blockchain_property_id: Optional[str] = None
     blockchain_owner_id: Optional[str] = None
     is_nft_minted: Optional[bool] = False
     transaction_hash: Optional[str] = None
@@ -57,7 +57,7 @@ class PropertyShow(PropertyBase):
 class PropertyUpdate(BaseModel):
     title: Optional[str] = None
     description: Optional[str] = None
-    blockchain_property_id: Optional[int] = None
+    blockchain_property_id: Optional[str] = None
     location: Optional[str] = None
     price: Optional[float] = None
     property_type: Optional[PropertyTypeEnum] = None
